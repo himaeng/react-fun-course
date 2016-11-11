38460e4d5d9dd4b29ba048e4c5c45d972fc18325
Tanakorn Numrubporn <him_aeng@yahoo.co.th>
2016-11-11 21:00:23 +0800

    finish template string exercise

---
+++ ES04-template-strings/app/containers/ConfirmBattleContainer.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import ConfirmBattle from '../components/ConfirmBattle'
+import githubHelpers from '../utils/githubHelpers'
+
+const ConfirmBattleContainer = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+  getInitialState () {
+    return {
+      isLoading: true,
+      playersInfo: [],
+    }
+  },
+  componentDidMount () {
+    const { query } = this.props.location;
+    githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+      .then((players) => {
+        this.setState({
+          isLoading: false,
+          playersInfo: [players[0], players[1]]
+        })
+      })
+  },
+  handleInitiateBattle () {
+    this.context.router.push({
+      pathname: '/results',
+      state: {
+        playersInfo: this.state.playersInfo
+      }
+    })
+  },
+  render () {
+    return (
+      <ConfirmBattle
+        isLoading={this.state.isLoading}
+        onInitiateBattle={this.handleInitiateBattle}
+        playersInfo={this.state.playersInfo} />
+    )
+  }
+});
+
+export default ConfirmBattleContainer
---
+++ ES04-template-strings/app/containers/ResultsContainer.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Results from '../components/Results'
+import githubHelpers from '../utils/githubHelpers'
+
+const ResultsContainer = React.createClass({
+  getInitialState () {
+    return {
+      isLoading: true,
+      scores: []
+    }
+  },
+  componentDidMount () {
+    githubHelpers.battle(this.props.location.state.playersInfo)
+      .then((scores) => {
+        this.setState({
+          scores: scores,
+          isLoading: false
+        })
+      })
+  },
+  render() {
+    return (
+      <Results
+        isLoading={this.state.isLoading}
+        playersInfo={this.props.location.state.playersInfo}
+        scores={this.state.scores}/>
+    )
+  }
+})
+
+export default ResultsContainer
